a3df04216afe9e8d564f9cc9a8c8d0cbca22747e
Minjae21 <csjae21@gmail.com>
2024-05-22 21:37:54 -0500

    5/22/24 ts file add

--- index.js
+++ index.js
@@ -1,137 +1,118 @@
-import { dictionary } from "./globals.js";
-
+const dictionary = ['apple', 'plane', 'crane'];
 const state = {
-    secret: dictionary[Math.floor(Math.random() * dictionary.length)], // correct word, random from dictionary
-    grid: Array(9) // row arr
-        .fill()
-        .map(() => Array(5).fill('')), // col arr
+    secret: dictionary[Math.floor(Math.random() * dictionary.length)],
+    grid: Array(9)
+        .fill('')
+        .map(() => Array(5).fill('')),
     currentRow: 0,
-    currentCol: 0, // pos in the grid where next letter can be typed
+    currentCol: 0,
 };
-
 function updateGrid() {
-    for(let i = 0; i < state.grid.length; i++) {
-        for(let j = 0; j < state.grid[i].length; j++) {
+    for (let i = 0; i < state.grid.length; i++) {
+        for (let j = 0; j < state.grid[i].length; j++) {
             const box = document.getElementById(`box${i}${j}`);
             box.textContent = state.grid[i][j];
         }
     }
 }
-
-function drawBox(container, row, col, letter = '') { // creating the box
+function drawBox(container, row, col, letter = '') {
     const box = document.createElement('div');
     box.className = 'box';
     box.id = `box${row}${col}`;
     box.textContent = letter;
-
     container.appendChild(box);
     return box;
 }
-
 function drawGrid(container) {
     const grid = document.createElement('div');
     grid.className = 'grid';
-
-    for(let i = 0; i < 9; i++) { // rows
-        for(let j = 0; j < 5; j++) {   // cols
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 5; j++) {
             drawBox(grid, i, j);
         }
     }
-
     container.appendChild(grid);
 }
-
-function keyboardEvents() {
+function registerEvents() {
     document.body.onkeydown = (e) => {
-        const key = e.key; // e, event object
-        if(key === 'Enter') {
-            if(state.currentCol === 5) { // check if all cols are filled
+        const key = e.key;
+        if (key === 'Enter') {
+            if (state.currentCol === 5) {
                 const word = getCurrentWord();
-                if(isWordValid(word)) {
+                if (isWordValid(word)) {
                     revealWord(word);
                     state.currentRow++;
-                    state.currentCol = 0; // move to the next row, set col 0
-                } else {
-                    alert('Not a real word!');
+                    state.currentCol = 0;
+                }
+                else {
+                    alert('Not a word');
                 }
             }
         }
-        if(key === 'Backspace') {
+        if (key === 'Backspace') {
             removeLetter();
         }
-        if(isLetter(key)) {
+        if (isLetter(key)) {
             addLetter(key);
         }
-
         updateGrid();
     };
 }
-
 function getCurrentWord() {
     return state.grid[state.currentRow].reduce((prev, curr) => prev + curr);
 }
-
 function isWordValid(word) {
     return dictionary.includes(word);
 }
-
 function revealWord(guess) {
     const row = state.currentRow;
     const animation_duration = 500;
-
-    for(let i = 0; i < 5; i++) { // loop over the letters
+    for (let i = 0; i < 5; i++) {
         const box = document.getElementById(`box${row}${i}`);
         const letter = box.textContent;
-
         setTimeout(() => {
-            if(letter === state.secret[i]) {
+            if (letter === state.secret[i]) {
                 box.classList.add('right');
-            } else if(state.secret.includes(letter)) {
+            }
+            else if (state.secret.includes(letter)) {
                 box.classList.add('wrong');
-            } else {
+            }
+            else {
                 box.classList.add('empty');
             }
         }, ((i + 1) * animation_duration) / 2);
-
         box.classList.add('animated');
         box.style.animationDelay = `${(i * animation_duration) / 2}ms`;
     }
-
-    const isWinner = state.secret === guess; // check if the user guesses the word
-    const isGameOver = state.currentRow === 8; // game over when the row reaches 9
-
+    const isWinner = state.secret === guess;
+    const isGameOver = state.currentRow === 8;
     setTimeout(() => {
-        if(isWinner) {
-            alert('Congrats!');
-        } else if(isGameOver) {
-            alert(`The correct word was ${state.secret}.`);
+        if (isWinner) {
+            alert('Congts!');
+        }
+        else {
+            alert(`Better luck next time, the answer was ${state.secret}.`);
         }
     }, 3 * animation_duration);
 }
-
 function isLetter(key) {
     return key.length === 1 && key.match(/[a-z]/i);
 }
-
 function addLetter(letter) {
-    if(state.currentCol === 5) return; // space check
-    state.grid[state.currentRow][state.currentCol] = letter; // otherwise, set the letter on the current pos
+    if (state.currentCol === 5)
+        return;
+    state.grid[state.currentRow][state.currentCol] = letter;
     state.currentCol++;
 }
-
 function removeLetter() {
-    if(state.currentCol === 0) return; // check if anything to remove
+    if (state.currentCol === 0)
+        return;
     state.grid[state.currentRow][state.currentCol - 1] = '';
     state.currentCol--;
 }
-
-function start() {
+function gameStart() {
     const game = document.getElementById('game');
     drawGrid(game);
-
-    keyboardEvents();
-
-    // console.log(state.secret);
+    registerEvents();
 }
-
-start();
+gameStart();
--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
-import { dictionary } from "./globals.js";
+const dictionary = ['apple', 'plane', 'crane'];
 
 const state = {
-    secret: dictionary[Math.floor(Math.random() * dictionary.length)], // correct word, random from dictionary
-    grid: Array(9) // row arr
-        .fill()
-        .map(() => Array(5).fill('')), // col arr
+    secret: dictionary[Math.floor(Math.random() * dictionary.length)],
+    grid: Array(9)
+        .fill('')
+        .map(() => Array(5).fill('')),
     currentRow: 0,
-    currentCol: 0, // pos in the grid where next letter can be typed
+    currentCol: 0,
 };
 
 function updateGrid() {
     for(let i = 0; i < state.grid.length; i++) {
-        for(let j = 0; j < state.grid[i].length; j++) {
+        for(let j = 0; j <  state.grid[i].length; j++) {
             const box = document.getElementById(`box${i}${j}`);
             box.textContent = state.grid[i][j];
         }
     }
 }
 
-function drawBox(container, row, col, letter = '') { // creating the box
+function drawBox(container, row : Number, col : Number, letter = '') {
     const box = document.createElement('div');
     box.className = 'box';
     box.id = `box${row}${col}`;
@@ -32,8 +32,8 @@ function drawGrid(container) {
     const grid = document.createElement('div');
     grid.className = 'grid';
 
-    for(let i = 0; i < 9; i++) { // rows
-        for(let j = 0; j < 5; j++) {   // cols
+    for(let i = 0; i < 9; i++) {
+        for(let j = 0; j < 5; j++) {
             drawBox(grid, i, j);
         }
     }
@@ -41,18 +41,18 @@ function drawGrid(container) {
     container.appendChild(grid);
 }
 
-function keyboardEvents() {
+function registerEvents() {
     document.body.onkeydown = (e) => {
-        const key = e.key; // e, event object
+        const key = e.key;
         if(key === 'Enter') {
-            if(state.currentCol === 5) { // check if all cols are filled
+            if(state.currentCol === 5) {
                 const word = getCurrentWord();
                 if(isWordValid(word)) {
                     revealWord(word);
                     state.currentRow++;
-                    state.currentCol = 0; // move to the next row, set col 0
+                    state.currentCol = 0;
                 } else {
-                    alert('Not a real word!');
+                    alert('Not a word');
                 }
             }
         }
@@ -79,7 +79,7 @@ function revealWord(guess) {
     const row = state.currentRow;
     const animation_duration = 500;
 
-    for(let i = 0; i < 5; i++) { // loop over the letters
+    for(let i = 0; i < 5; i++) {
         const box = document.getElementById(`box${row}${i}`);
         const letter = box.textContent;
 
@@ -97,14 +97,14 @@ function revealWord(guess) {
         box.style.animationDelay = `${(i * animation_duration) / 2}ms`;
     }
 
-    const isWinner = state.secret === guess; // check if the user guesses the word
-    const isGameOver = state.currentRow === 8; // game over when the row reaches 9
+    const isWinner = state.secret === guess;
+    const isGameOver = state.currentRow === 8;
 
     setTimeout(() => {
         if(isWinner) {
-            alert('Congrats!');
-        } else if(isGameOver) {
-            alert(`The correct word was ${state.secret}.`);
+            alert('Congts!');
+        } else {
+            alert(`Better luck next time, the answer was ${state.secret}.`);
         }
     }, 3 * animation_duration);
 }
@@ -114,24 +114,22 @@ function isLetter(key) {
 }
 
 function addLetter(letter) {
-    if(state.currentCol === 5) return; // space check
-    state.grid[state.currentRow][state.currentCol] = letter; // otherwise, set the letter on the current pos
+    if(state.currentCol === 5) return;
+    state.grid[state.currentRow][state.currentCol] = letter;
     state.currentCol++;
 }
 
 function removeLetter() {
-    if(state.currentCol === 0) return; // check if anything to remove
+    if(state.currentCol === 0) return;
     state.grid[state.currentRow][state.currentCol - 1] = '';
     state.currentCol--;
 }
 
-function start() {
+function gameStart() {
     const game = document.getElementById('game');
     drawGrid(game);
 
-    keyboardEvents();
-
-    // console.log(state.secret);
+    registerEvents();
 }
 
-start();
+gameStart();
